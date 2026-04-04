6641999b84eb090da315a3474d6abbfac5f8fa77
Voevodin Kirill <voev.kirill@gmail.com>
2026-04-04 19:25:23 +0300

    unix socket implementation

--- include/casket/utils/error_code.hpp
+++ include/casket/utils/error_code.hpp
@@ -14,4 +14,9 @@ inline void ClearError(std::error_code& ec)
     ec.assign(0, ec.category());
 }
 
+inline void SetSystemError(std::error_code& ec, const std::errc e)
+{
+    ec = std::make_error_code(e);
+}
+
 } // namespace casket
