4ea30c8d53bd61d64a2d9487ca76c554342e072e
Voevodin Kirill <voev.kirill@gmail.com>
2026-04-12 13:51:03 +0300

    use byte buffer to handling requests

---
+++ include/casket/types/byte_buffer.hpp
@@ -0,0 +1,216 @@
+#pragma once
+
+#include <memory>
+#include <cstring>
+#include <cstdint>
+
+namespace casket
+{
+
+class ByteBuffer
+{
+public:
+    static constexpr size_t DEFAULT_SIZE = 8192;
+
+    explicit ByteBuffer(size_t size = DEFAULT_SIZE)
+        : data_(std::make_unique<uint8_t[]>(size))
+        , size_(size)
+    {
+    }
+
+    // Базовые методы
+    uint8_t* data()
+    {
+        return data_.get();
+    }
+    const uint8_t* data() const
+    {
+        return data_.get();
+    }
+    size_t size() const
+    {
+        return size_;
+    }
+
+    // Позиции (виртуальные, для кольцевого буфера)
+    size_t writePos() const
+    {
+        return writePos_;
+    }
+    size_t readPos() const
+    {
+        return readPos_;
+    }
+
+    // Реальные индексы в массиве
+    size_t writeIndex() const
+    {
+        return writePos_ % size_;
+    }
+    size_t readIndex() const
+    {
+        return readPos_ % size_;
+    }
+
+    // Доступные для записи байты
+    size_t available() const
+    {
+        return size_ - (writePos_ - readPos_);
+    }
+
+    // Доступные для чтения байты
+    size_t readable() const
+    {
+        return writePos_ - readPos_;
+    }
+
+    // Очистка (без освобождения памяти)
+    void clear()
+    {
+        writePos_ = 0;
+        readPos_ = 0;
+    }
+
+    void reset()
+    {
+        clear();
+    }
+
+    // Запись данных (возвращает сколько записано)
+    size_t write(const uint8_t* src, size_t len)
+    {
+        size_t written = 0;
+
+        while (written < len && available() > 0)
+        {
+            size_t idx = writeIndex();
+            size_t spaceToEnd = size_ - idx;
+            size_t toWrite = std::min(len - written, spaceToEnd);
+
+            // Проверяем, не упремся ли в read pointer
+            size_t maxWrite = size_ - readable();
+            if (toWrite > maxWrite)
+            {
+                toWrite = maxWrite;
+            }
+
+            if (toWrite == 0)
+                break;
+
+            memcpy(data_.get() + idx, src + written, toWrite);
+            writePos_ += toWrite;
+            written += toWrite;
+        }
+
+        return written;
+    }
+
+    // Чтение данных (возвращает сколько прочитано)
+    size_t read(uint8_t* dst, size_t len)
+    {
+        size_t read = 0;
+
+        while (read < len && readable() > 0)
+        {
+            size_t idx = readIndex();
+            size_t dataToEnd = size_ - idx;
+            size_t toRead = std::min(len - read, dataToEnd);
+
+            // Не читаем больше чем есть
+            if (toRead > readable())
+            {
+                toRead = readable();
+            }
+
+            if (toRead == 0)
+                break;
+
+            memcpy(dst + read, data_.get() + idx, toRead);
+            readPos_ += toRead;
+            read += toRead;
+        }
+
+        return read;
+    }
+
+    // Прямая запись (возвращает указатель на место для записи)
+    uint8_t* prepareWrite(size_t& available_out)
+    {
+        size_t idx = writeIndex();
+        size_t spaceToEnd = size_ - idx;
+        size_t maxWrite = size_ - readable();
+
+        available_out = std::min(spaceToEnd, maxWrite);
+        if (available_out == 0)
+            return nullptr;
+
+        return data_.get() + idx;
+    }
+
+    void commit(size_t bytes)
+    {
+        writePos_ += bytes;
+    }
+
+    // Прямое чтение (возвращает указатель на данные)
+    uint8_t* prepareRead(size_t& available_out)
+    {
+        size_t idx = readIndex();
+        size_t dataToEnd = size_ - idx;
+
+        available_out = std::min(dataToEnd, readable());
+        if (available_out == 0)
+            return nullptr;
+
+        return data_.get() + idx;
+    }
+
+    void consume(size_t bytes)
+    {
+        readPos_ += bytes;
+    }
+
+    // Пропустить данные при чтении
+    bool skip(size_t bytes)
+    {
+        if (readable() < bytes)
+            return false;
+        readPos_ += bytes;
+        return true;
+    }
+
+    // Прочитать без копирования (возвращает view на данные)
+    uint8_t* peek(size_t& available_out)
+    {
+        return prepareRead(available_out);
+    }
+
+    // Получить линейный буфер для последовательного чтения (если нужно)
+    size_t linearRead(uint8_t*& out_ptr)
+    {
+        size_t idx = readIndex();
+        size_t dataToEnd = size_ - idx;
+        size_t avail = std::min(dataToEnd, readable());
+        out_ptr = data_.get() + idx;
+        return avail;
+    }
+
+    // Получить линейный буфер для последовательной записи
+    size_t linearWrite(uint8_t*& out_ptr)
+    {
+        size_t idx = writeIndex();
+        size_t spaceToEnd = size_ - idx;
+        size_t maxWrite = size_ - readable();
+        size_t avail = std::min(spaceToEnd, maxWrite);
+        out_ptr = data_.get() + idx;
+        return avail;
+    }
+
+private:
+    std::unique_ptr<uint8_t[]> data_;
+    size_t size_{0};
+    size_t writePos_{0}; // виртуальная позиция записи
+    size_t readPos_{0};  // виртуальная позиция чтения
+};
+
+} // namespace casket
