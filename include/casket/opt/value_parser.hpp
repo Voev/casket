3669eea2b796ae338ca346c11e5a94f9fdc54a29
Voevodin Kirill <voev.kirill@gmail.com>
2026-05-01 13:07:06 +0300

    value parser specialization for std::string

--- include/casket/opt/value_parser.hpp
+++ include/casket/opt/value_parser.hpp
@@ -25,6 +25,15 @@ struct ValueParser
 };
 
 template <>
+struct ValueParser<std::string>
+{
+    static std::string parse(const std::string& str)
+    {
+        return str;
+    }
+};
+
+template <>
 struct ValueParser<bool>
 {
     static bool parse(const std::string& str)
