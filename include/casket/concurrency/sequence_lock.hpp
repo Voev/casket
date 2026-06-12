5029fa703e4422f4753190ec679bbffb12ff3988
Voev <voev.kirill@gmail.com>
2026-06-12 19:12:50 +0300

    lf fixes (#47)

--- include/casket/concurrency/sequence_lock.hpp
+++ include/casket/concurrency/sequence_lock.hpp
@@ -1,82 +1,57 @@
 #pragma once
 #include <atomic>
-#include <cstddef>
-#include <cstdint>
 #include <type_traits>
-#include <thread>
 
 namespace casket
 {
 
-constexpr size_t CACHE_LINE_SIZE = 64;
-
-#ifdef __x86_64__
-#define CPU_PAUSE() __builtin_ia32_pause()
-#elif defined(__arm__) || defined(__aarch64__)
-#define CPU_PAUSE() asm volatile("yield" ::: "memory")
-#else
-#define CPU_PAUSE() std::this_thread::yield()
-#endif
-
 template <typename T>
-class SequenceLock final
+class SequenceLock
 {
-private:
-    static_assert(std::is_trivially_copyable_v<T>, "T must be trivially copyable");
-
-    alignas(CACHE_LINE_SIZE) std::atomic<uint32_t> seq_{0};
-    alignas(CACHE_LINE_SIZE) T value_;
-
 public:
-    SequenceLock() = default;
-    ~SequenceLock() = default;
+    static_assert(std::is_nothrow_copy_assignable<T>::value, "T must satisfy is_nothrow_copy_assignable");
+    static_assert(std::is_trivially_copy_assignable<T>::value, "T must satisfy is_trivially_copy_assignable");
 
-    SequenceLock(const SequenceLock&) = delete;
-    SequenceLock& operator=(const SequenceLock&) = delete;
-
-    void store(const T& value) noexcept
+    SequenceLock()
+        : seq_(0)
     {
-        auto seq = seq_.load(std::memory_order_acquire);
-        seq_.store(seq + 1, std::memory_order_release);
-
-        std::atomic_thread_fence(std::memory_order_release);
-        value_ = value;
-        std::atomic_thread_fence(std::memory_order_release);
-
-        seq_.store(seq + 2, std::memory_order_release);
     }
 
-    void load(T& value) const noexcept
+    T load() const noexcept
     {
-        uint32_t seq;
-        uint32_t spins = 0;
-
+        T copy;
+        std::size_t seq0, seq1;
         do
         {
-            seq = seq_.load(std::memory_order_acquire);
-
-            if (seq & 1)
-            {
-                auto min = 1u << std::min(10u, spins);
-                for (uint32_t i = 0; i < min; ++i)
-                {
-                    CPU_PAUSE();
-                }
-                if (spins < 10u)
-                {
-                    spins++;
-                }
-                continue;
-            }
-
-            spins = 0;
-
-            std::atomic_thread_fence(std::memory_order_acquire);
-            value = value_;
-            std::atomic_thread_fence(std::memory_order_acquire);
-
-        } while (seq != seq_.load(std::memory_order_acquire));
+            seq0 = seq_.load(std::memory_order_acquire);
+            std::atomic_signal_fence(std::memory_order_acq_rel);
+            copy = value_;
+            std::atomic_signal_fence(std::memory_order_acq_rel);
+            seq1 = seq_.load(std::memory_order_acquire);
+        } while (seq0 != seq1 || seq0 & 1);
+        return copy;
     }
+
+    void store(const T& desired) noexcept
+    {
+        std::size_t seq0 = seq_.load(std::memory_order_relaxed);
+        seq_.store(seq0 + 1, std::memory_order_release);
+        std::atomic_signal_fence(std::memory_order_acq_rel);
+        value_ = desired;
+        std::atomic_signal_fence(std::memory_order_acq_rel);
+        seq_.store(seq0 + 2, std::memory_order_release);
+    }
+
+private:
+    static const std::size_t kFalseSharingRange = 128;
+
+    // Align to prevent false sharing with adjecent data
+    alignas(kFalseSharingRange) T value_;
+    std::atomic<std::size_t> seq_;
+    // Padding to prevent false sharing with adjecent data
+    char padding_[kFalseSharingRange - ((sizeof(value_) + sizeof(seq_)) % kFalseSharingRange)];
+    static_assert(((sizeof(value_) + sizeof(seq_) + sizeof(padding_)) % kFalseSharingRange) == 0,
+                  "sizeof(Seqlock<T>) should be a multiple of kFalseSharingRange");
 };
 
 } // namespace casket
